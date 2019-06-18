76a967c19862fbe061d9b6b6b50eb646875c1811
vuducchinh29 <vuducchinh29@gmail.com>
2019-06-18 17:55:31 +0700

    Create new MusicHub project by Springboot

---
+++ src/main/java/codegym/cdkteam/musichub/MusichubApplication.java
@@ -0,0 +1,13 @@
+package codegym.cdkteam.musichub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusichubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusichubApplication.class, args);
+	}
+
+}
