b15cbf3cc4706fbf54db51b858c79169f95440c3
khoipn-dev <khoipn.dev@gmail.com>
2019-06-19 09:15:15 +0700

    create method verify resetPassword

--- src/main/java/codegym/cdkteam/musichub/controller/VerifyController.java
+++ src/main/java/codegym/cdkteam/musichub/controller/VerifyController.java
@@ -5,11 +5,16 @@ import codegym.cdkteam.musichub.model.UserDTO;
 import codegym.cdkteam.musichub.service.TokenVerifyDTOService;
 import codegym.cdkteam.musichub.service.UserDTOService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 @Controller
@@ -36,5 +41,32 @@ public class VerifyController {
       }
       return "redirect:/";
     }
+  }
+
+  @GetMapping("/user/resetPassword")
+  public String resetPassword(@RequestParam(name = "id") Optional<Long> userId, @RequestParam Optional<String> token) {
+    if (!(userId.isPresent() || token.isPresent())) return "redirect:/404";
+
+    String result = validatePasswordResetToken(userId.get(), token.get());
+    if (result != null) {
+      return "redirect:/404";
+    }
+    return "redirect:/user/updatePassword";
+  }
+
+  public String validatePasswordResetToken(long userId, String token) {
+    TokenVerifyDTO passToken = tokenVerifyDTOService.findByUserId(userId);
+    if (passToken == null) {
+      return "invalidToken";
+    }
+    Boolean result = token.equals(passToken.getToken());
+    if (!result) {
+      return "invalidToken";
+    }
+    UserDTO user = userDTOService.findById(userId).get();
+    Authentication auth = new UsernamePasswordAuthenticationToken(user, null, Arrays.asList(
+            new SimpleGrantedAuthority("CHANGE_PASSWORD_PRIVILEGE")));
+    SecurityContextHolder.getContext().setAuthentication(auth);
+    return null;
   }
 }
