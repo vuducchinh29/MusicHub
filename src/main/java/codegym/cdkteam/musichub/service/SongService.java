d643fb8096c61273f05e5310371e6ad6efc0b7b9
vuducchinh29 <vuducchinh29@gmail.com>
2019-06-24 09:43:35 +0700

    Add methods findAll, findById, save and delete song

--- src/main/java/codegym/cdkteam/musichub/service/SongService.java
+++ src/main/java/codegym/cdkteam/musichub/service/SongService.java
@@ -1,4 +1,13 @@
 package codegym.cdkteam.musichub.service;
 
+import codegym.cdkteam.musichub.model.song.Song;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface SongService {
+  List<Song> findAll();
+  Optional<Song> findById(long id);
+  Song save(Song song);
+  void delete(long id);
 }
