8889efdc307afa94571e61c784558dc4937f4eee
vuducchinh29 <vuducchinh29@gmail.com>
2019-06-24 10:07:16 +0700

    Autowired Song service

--- src/main/java/codegym/cdkteam/musichub/controller/SongController.java
+++ src/main/java/codegym/cdkteam/musichub/controller/SongController.java
@@ -1,7 +1,11 @@
 package codegym.cdkteam.musichub.controller;
 
+import codegym.cdkteam.musichub.service.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SongController {
+  @Autowired
+  SongService songService;
 }
