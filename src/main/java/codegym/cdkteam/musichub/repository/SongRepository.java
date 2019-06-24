947b328b2015ca0bbd3457c1a49170e6cecfce87
vuducchinh29 <vuducchinh29@gmail.com>
2019-06-24 09:50:21 +0700

    Remove method findById

--- src/main/java/codegym/cdkteam/musichub/repository/SongRepository.java
+++ src/main/java/codegym/cdkteam/musichub/repository/SongRepository.java
@@ -4,5 +4,4 @@ import codegym.cdkteam.musichub.model.song.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SongRepository extends JpaRepository<Song, Long> {
-  Song findById(long id);
 }
