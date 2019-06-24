96a3d6477ca8ea64e698bf3c746982cc37e57d3e
vuducchinh29 <vuducchinh29@gmail.com>
2019-06-24 10:10:00 +0700

    Add getmapping controller to create addNewSong

--- src/main/java/codegym/cdkteam/musichub/controller/SongController.java
+++ src/main/java/codegym/cdkteam/musichub/controller/SongController.java
@@ -1,11 +1,20 @@
 package codegym.cdkteam.musichub.controller;
 
+import codegym.cdkteam.musichub.model.song.Song;
 import codegym.cdkteam.musichub.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SongController {
   @Autowired
   SongService songService;
+
+  @GetMapping("/admin/add-new-song")
+  public String addNewSong(Model model){
+    model.addAttribute("song", new Song());
+    return "/song/addNewSong";
+  }
 }
