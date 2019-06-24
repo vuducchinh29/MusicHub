cf59e28b8d6e4da930deec85b09f3125654534d7
vuducchinh29 <vuducchinh29@gmail.com>
2019-06-24 09:31:11 +0700

    Add method find song by id

--- src/main/java/codegym/cdkteam/musichub/repository/SongRepository.java
+++ src/main/java/codegym/cdkteam/musichub/repository/SongRepository.java
@@ -4,4 +4,5 @@ import codegym.cdkteam.musichub.model.song.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SongRepository extends JpaRepository<Song, Long> {
+  Song findById(long id);
 }
