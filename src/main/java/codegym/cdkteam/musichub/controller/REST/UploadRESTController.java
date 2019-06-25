c069911e771da86a429ad072f7614c5ae1cfc9d7
khoipn-dev <khoipn.dev@gmail.com>
2019-06-25 12:00:23 +0700

    create controller uploadRESTController and add method uploadSong

---
+++ src/main/java/codegym/cdkteam/musichub/controller/REST/UploadRESTController.java
@@ -0,0 +1,25 @@
+package codegym.cdkteam.musichub.controller.REST;
+
+import codegym.cdkteam.musichub.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+public class UploadRESTController {
+  @Autowired
+  private StorageService storageService;
+
+  @PostMapping(value = "/uploadSong", consumes = {"multipart/form-data"})
+  public ResponseEntity uploadSong(@RequestParam MultipartFile file) {
+    try {
+      String link = storageService.uploadFile(file, StorageService.SONG_LOCATION);
+      return ResponseEntity.status(HttpStatus.OK).body(link);
+    } catch (Exception e) {
+      String message = "FAIL to upload " + file.getOriginalFilename();
+      return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+    }
+  }
+}
