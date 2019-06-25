01e25cb4cf0eeec00ba253bf75c0b527d35f9f2b
khoipn-dev <khoipn.dev@gmail.com>
2019-06-25 11:55:53 +0700

    impl method loadFile

--- src/main/java/codegym/cdkteam/musichub/service/impl/StorageServiceImpl.java
+++ src/main/java/codegym/cdkteam/musichub/service/impl/StorageServiceImpl.java
@@ -3,8 +3,10 @@ package codegym.cdkteam.musichub.service.impl;
 import codegym.cdkteam.musichub.service.StorageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
