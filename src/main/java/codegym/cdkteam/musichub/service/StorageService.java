2ae8c537077f8d11c307ed5fdaafac4546fed059
khoipn-dev <khoipn.dev@gmail.com>
2019-06-25 10:58:04 +0700

    update interface storageservice

--- src/main/java/codegym/cdkteam/musichub/service/StorageService.java
+++ src/main/java/codegym/cdkteam/musichub/service/StorageService.java
@@ -4,8 +4,11 @@ import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public interface StorageService {
+  Path SONG_LOCATION = Paths.get("files/song");
+  Path IMAGE_LOCATION = Paths.get("files/image");
   String uploadFile(MultipartFile file, Path location);
   Resource loadFile(String fileName, Path location);
 }
