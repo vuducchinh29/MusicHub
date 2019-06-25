a54b6ab883e71fc006f26e04f094b2dee9041a7c
khoipn-dev <khoipn.dev@gmail.com>
2019-06-25 12:02:18 +0700

    add method getSong

--- src/main/java/codegym/cdkteam/musichub/controller/REST/UploadRESTController.java
+++ src/main/java/codegym/cdkteam/musichub/controller/REST/UploadRESTController.java
@@ -2,8 +2,12 @@ package codegym.cdkteam.musichub.controller.REST;
 
 import codegym.cdkteam.musichub.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,5 +36,13 @@ public class UploadRESTController {
       String message = "FAIL to upload " + file.getOriginalFilename();
       return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
     }
+  }
+
+  @GetMapping("/files/song/{filename:.+}")
+  public ResponseEntity<Resource> getSong(@PathVariable String filename) {
+    Resource file = storageService.loadFile(filename, StorageService.SONG_LOCATION);
+    return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+            .body(file);
   }
 }
