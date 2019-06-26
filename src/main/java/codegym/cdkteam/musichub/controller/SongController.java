70e43aaaac9eb981d62a75841b6b94c14a32bdc2
vuducchinh29 <vuducchinh29@gmail.com>
2019-06-26 11:59:26 +0700

    add get mapping to show song detail

--- src/main/java/codegym/cdkteam/musichub/controller/SongController.java
+++ src/main/java/codegym/cdkteam/musichub/controller/SongController.java
@@ -8,8 +8,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class SongController {
@@ -35,4 +38,20 @@ public class SongController {
     modelAndView.addObject("message", "Upload success!");
     return modelAndView;
   }
+
+  @GetMapping("/song-detail/{id}")
+  public ModelAndView showSongDetail(@PathVariable long id){
+    Song song = songService.findById(id).get();
+    ModelAndView modelAndView = new ModelAndView("song/details");
+    modelAndView.addObject("song", song);
+    return modelAndView;
+  }
+
+//  @GetMapping("/")
+//  public ModelAndView showAllSong() {
+//    List<Song> songs = songService.findAll();
+//    ModelAndView modelAndView = new ModelAndView("song/index");
+//    modelAndView.addObject("songs", songs);
+//    return modelAndView;
+//  }
 }
