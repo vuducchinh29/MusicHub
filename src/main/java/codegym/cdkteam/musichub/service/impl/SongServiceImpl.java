d84b7a078e3b5b87a025e8b0b146f7c432f69ac8
ducnv1999 <ducgau99@gmail.com>
2019-07-03 15:37:39 +0700

    update SongServiceImpl

--- src/main/java/codegym/cdkteam/musichub/service/impl/SongServiceImpl.java
+++ src/main/java/codegym/cdkteam/musichub/service/impl/SongServiceImpl.java
@@ -4,18 +4,20 @@ import codegym.cdkteam.musichub.model.song.Song;
 import codegym.cdkteam.musichub.repository.SongRepository;
 import codegym.cdkteam.musichub.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 @Service
 public class SongServiceImpl implements SongService {
   @Autowired
   SongRepository songRepository;
 
+
   @Override
-  public List<Song> findAll() {
-    return songRepository.findAll();
+  public Page<Song> findAll(Pageable pageable) {
+    return songRepository.findAll(pageable);
   }
 
   @Override
