4de50f23e69e781bc5878de6abe32a7eae7c774b
ducnv1999 <ducgau99@gmail.com>
2019-07-03 15:33:18 +0700

    update SongService

--- src/main/java/codegym/cdkteam/musichub/service/SongService.java
+++ src/main/java/codegym/cdkteam/musichub/service/SongService.java
@@ -1,12 +1,13 @@
 package codegym.cdkteam.musichub.service;
 
 import codegym.cdkteam.musichub.model.song.Song;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface SongService {
-  List<Song> findAll();
+  Page<Song> findAll(Pageable pageable);
   Optional<Song> findById(long id);
   Song save(Song song);
   void delete(long id);
