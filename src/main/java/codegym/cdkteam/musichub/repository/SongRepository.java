18e419de3f3d7f0294efa797ace0d58612650b34
ducnv1999 <ducgau99@gmail.com>
2019-07-03 16:00:03 +0700

    update SongRepository

--- src/main/java/codegym/cdkteam/musichub/repository/SongRepository.java
+++ src/main/java/codegym/cdkteam/musichub/repository/SongRepository.java
@@ -1,7 +1,10 @@
 package codegym.cdkteam.musichub.repository;
 
 import codegym.cdkteam.musichub.model.song.Song;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SongRepository extends JpaRepository<Song, Long> {
+    Page<Song>findAllByNameContaining(String name, Pageable pageable);
 }
