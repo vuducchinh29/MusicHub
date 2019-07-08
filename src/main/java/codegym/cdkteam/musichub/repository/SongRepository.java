a2d799b41f945551ed3d7a42654736a476895d85
khoipn-dev <khoipn.dev@gmail.com>
2019-07-08 14:28:52 +0700

    edit repository

--- src/main/java/codegym/cdkteam/musichub/repository/SongRepository.java
+++ src/main/java/codegym/cdkteam/musichub/repository/SongRepository.java
@@ -1,10 +1,10 @@
 package codegym.cdkteam.musichub.repository;
 
-import codegym.cdkteam.musichub.model.song.Song;
+import codegym.cdkteam.musichub.model.song.SongDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SongRepository extends JpaRepository<Song, Long> {
-    Page<Song>findAllByNameContaining(String name, Pageable pageable);
+public interface SongRepository extends JpaRepository<SongDTO, Long> {
+    Page<SongDTO>findAllByNameContaining(String name, Pageable pageable);
 }
