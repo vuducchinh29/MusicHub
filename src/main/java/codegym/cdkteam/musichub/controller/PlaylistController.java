aa028b9fff9b7e4198a3e665cfaa0df832ec09aa
vuducchinh29 <vuducchinh29@gmail.com>
2019-07-08 10:03:43 +0700

    Create method Get to create playlist

--- src/main/java/codegym/cdkteam/musichub/controller/PlaylistController.java
+++ src/main/java/codegym/cdkteam/musichub/controller/PlaylistController.java
@@ -1,12 +1,15 @@
 package codegym.cdkteam.musichub.controller;
 
+import codegym.cdkteam.musichub.model.Playlist;
 import codegym.cdkteam.musichub.model.song.Song;
 import codegym.cdkteam.musichub.service.PlaylistService;
 import codegym.cdkteam.musichub.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -22,5 +25,12 @@ public class PlaylistController {
   @ModelAttribute("songs")
   public List<Song> songs() {
     return songService.findAll();
+  }
+
+  @GetMapping("/create")
+  public ModelAndView createNewPlaylist() {
+    ModelAndView modelAndView = new ModelAndView("playlist/addNewPlaylist");
+    modelAndView.addObject("playlist", new Playlist());
+    return modelAndView;
   }
 }
