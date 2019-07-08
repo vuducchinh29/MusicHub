7bed3dbe078e3064c220bb117411d25c4a489777
vuducchinh29 <vuducchinh29@gmail.com>
2019-07-08 09:52:30 +0700

    Implement PlaylistServiceImpl

---
+++ src/main/java/codegym/cdkteam/musichub/service/impl/PlaylistServiceImpl.java
@@ -0,0 +1,53 @@
+package codegym.cdkteam.musichub.service.impl;
+
+import codegym.cdkteam.musichub.model.Playlist;
+import codegym.cdkteam.musichub.model.song.Song;
+import codegym.cdkteam.musichub.repository.PlaylistRepository;
+import codegym.cdkteam.musichub.service.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class PlaylistServiceImpl implements PlaylistService {
+  @Autowired
+  PlaylistRepository playlistRepository;
+
+  @Override
+  public List<Playlist> findAll() {
+    return playlistRepository.findAll();
+  }
+
+  @Override
+  public Optional<Playlist> findById(Long id) {
+    return playlistRepository.findById(id);
+  }
+
+  @Override
+  public void save(Playlist playlist) {
+    playlistRepository.save(playlist);
+  }
+
+  @Override
+  public void remove(Long id) {
+    playlistRepository.deleteById(id);
+  }
+
+  @Override
+  public List<Song> uncheckedSongs(List<Song> allsongs, List<Song> checkedsongs){
+    List<Song> uncheckedSongs = new ArrayList<>();
+    for (int i = 0; i < allsongs.size(); i++) {
+      boolean check = false;
+      for (int a = 0; a < checkedsongs.size(); a++) {
+        if (allsongs.get(i).getId() == checkedsongs.get(a).getId()) {
+          check = true;
+        }
+      }
+      if (!check) {
+        uncheckedSongs.add(allsongs.get(i));
+      }
+    }
+    return uncheckedSongs;
+  }
+}
