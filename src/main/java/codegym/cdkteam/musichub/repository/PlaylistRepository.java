d5e25be5c2c66932ac31cc0fe8f603713f34a6f1
vuducchinh29 <vuducchinh29@gmail.com>
2019-07-12 17:54:04 +0700

    Add method findAllByOrderByCreateAtDesc

--- src/main/java/codegym/cdkteam/musichub/repository/PlaylistRepository.java
+++ src/main/java/codegym/cdkteam/musichub/repository/PlaylistRepository.java
@@ -3,5 +3,8 @@ package codegym.cdkteam.musichub.repository;
 import codegym.cdkteam.musichub.model.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlaylistRepository extends JpaRepository<Playlist,Long> {
+  List<Playlist> findAllByOrderByCreatedAtDesc();
 }
