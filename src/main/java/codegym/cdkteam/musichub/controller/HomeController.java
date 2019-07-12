d9a16ffdc979af1bfbd03bc316943f2bde3f2056
vuducchinh29 <vuducchinh29@gmail.com>
2019-07-12 18:00:43 +0700

    Create HomeController

---
+++ src/main/java/codegym/cdkteam/musichub/controller/HomeController.java
@@ -0,0 +1,41 @@
+package codegym.cdkteam.musichub.controller;
+
+import codegym.cdkteam.musichub.model.Playlist;
+import codegym.cdkteam.musichub.model.song.SongDTO;
+import codegym.cdkteam.musichub.service.PlaylistService;
+import codegym.cdkteam.musichub.service.SongDTOService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+  @Autowired
+  SongDTOService songService;
+
+  @Autowired
+  PlaylistService playlistService;
+
+  @GetMapping("/home")
+  public ModelAndView showIndex(){
+    // Playlist moi nhat
+    List<Playlist> newestPlaylists = playlistService.findAllByOrderByCreatedAtDesc();
+    // Playlist nhieu luot click nhat
+    List<Playlist> mostPlayedPlaylists = playlistService.findAll();
+    // Bai hat moi nhat
+    List<SongDTO> newestSongs = songService.findAllByOrderByCreatedAtDesc();
+    // Bai hat nhieu luot nghe nhat
+    List<SongDTO> mostPlayedSongs = songService.findAll();
+
+    ModelAndView modelAndView = new ModelAndView("index");
+    modelAndView.addObject("newestPlaylists", newestPlaylists);
+    modelAndView.addObject("mostPlayedPlaylists", mostPlayedPlaylists);
+    modelAndView.addObject("newestSongs", newestSongs);
+    modelAndView.addObject("mostPlayedSongs", mostPlayedSongs);
+    return modelAndView;
+  }
+
+}
