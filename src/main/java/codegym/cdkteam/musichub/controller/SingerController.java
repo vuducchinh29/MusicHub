0d876919a3f77773f9777d430937ab2fbe2c4376
ducnv1999 <ducgau99@gmail.com>
2019-07-13 09:49:57 +0700

    add @GetMapping show list singer

--- src/main/java/codegym/cdkteam/musichub/controller/SingerController.java
+++ src/main/java/codegym/cdkteam/musichub/controller/SingerController.java
@@ -1,4 +1,34 @@
 package codegym.cdkteam.musichub.controller;
 
+import codegym.cdkteam.musichub.model.Singer;
+import codegym.cdkteam.musichub.service.SingerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/singers")
 public class SingerController {
+    @Autowired
+    private SingerService singerService;
+
+    @GetMapping("")
+    public ModelAndView listSinger(@RequestParam("s") Optional<String> s){
+        List<Singer> singers;
+        if(s.isPresent()){
+            singers = singerService.findAllByNameContaining(s.get());
+        } else {
+            singers = singerService.findAll();
+
+        }
+        ModelAndView modelAndView = new ModelAndView("singer/list");
+        modelAndView.addObject("singers", singers);
+        return modelAndView;
+    }
 }
