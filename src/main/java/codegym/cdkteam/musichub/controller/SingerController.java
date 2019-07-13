5e78ec537c5110847c648a6d21abbcf6df89f9d8
ducnv1999 <ducgau99@gmail.com>
2019-07-13 10:43:29 +0700

    add @PostMapping in create singer

--- src/main/java/codegym/cdkteam/musichub/controller/SingerController.java
+++ src/main/java/codegym/cdkteam/musichub/controller/SingerController.java
@@ -5,6 +5,8 @@ import codegym.cdkteam.musichub.service.SingerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -29,5 +31,12 @@ public class SingerController {
         modelAndView.addObject("singer", new Singer());
         return modelAndView;
     }
-
+    @PostMapping("/create")
+    public ModelAndView addNewSinger(@ModelAttribute("singer") Singer singer){
+        singerService.save(singer);
+        ModelAndView modelAndView = new ModelAndView("singer/create");
+        modelAndView.addObject("singer", new Singer());
+        modelAndView.addObject("message", "Create a successful singer");
+        return modelAndView;
+    }
 }
