fe6f006055ab3f57c8e8e3cac97507a9aeb01f6c
ducnv1999 <ducgau99@gmail.com>
2019-07-13 09:00:54 +0700

    add class SingerServiceImpl

--- src/main/java/codegym/cdkteam/musichub/service/impl/SingerServiceImpl.java
+++ src/main/java/codegym/cdkteam/musichub/service/impl/SingerServiceImpl.java
@@ -1,5 +1,36 @@
 package codegym.cdkteam.musichub.service.impl;
 
-public class SingerServiceImpl
-{
+import codegym.cdkteam.musichub.model.Singer;
+import codegym.cdkteam.musichub.repository.SingerRepository;
+import codegym.cdkteam.musichub.service.SingerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SingerServiceImpl implements SingerService {
+    @Autowired
+    SingerRepository singerRepository;
+    @Override
+    public List<Singer> findAll() {
+        return singerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Singer> findById(Long id) {
+        return singerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Singer singer) {
+        singerRepository.save(singer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        singerRepository.deleteById(id);
+    }
+
 }
